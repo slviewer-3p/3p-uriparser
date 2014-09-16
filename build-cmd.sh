1171d1d08ce22f5df587b34c5b611baab507500b
maksymsproductengine <maksymsproductengine@lindenlab.com>
2014-09-16 20:35:13 +0400

    For darwin: enabling doc and removing .exp files copying

--- build-cmd.sh
+++ build-cmd.sh
@@ -69,21 +69,17 @@ pushd "$URIPARSER_SOURCE_DIR"
             # Debug first
             CFLAGS="$opts -O0 -gdwarf-2 -fPIC -DPIC" \
                 LDFLAGS="-Wl,-install_name,\"${install_name}\" -Wl,-headerpad_max_install_names" \
-                ./configure --prefix="$stage" --includedir="$stage/include/uriparser" --libdir="$stage/lib/debug" --disable-test --disable-doc
+                ./configure --prefix="$stage" --includedir="$stage/include/uriparser" --libdir="$stage/lib/debug" --disable-test
             make
             make install
-            cp -a "$top"/uriparser_darwin_debug.exp "$stage"/lib/debug/uriparser_darwin.exp
-
             make distclean
 
             # Now release
             CFLAGS="$opts -O3 -gdwarf-2 -fPIC -DPIC" \
                 LDFLAGS="-Wl,-install_name,\"${install_name}\" -Wl,-headerpad_max_install_names" \
-                ./configure --prefix="$stage" --includedir="$stage/include/uriparser" --libdir="$stage/lib/release" --disable-test --disable-doc
+                ./configure --prefix="$stage" --includedir="$stage/include/uriparser" --libdir="$stage/lib/release" --disable-test
             make
             make install
-            cp -a "$top"/uriparser_darwin_release.exp "$stage"/lib/release/uriparser_darwin.exp
-
             make distclean
         ;;
 
